a9482d3468858cc0e9a034e71e7e6cf01029c475
wangze <974360400@qq.com>
2022-03-15 11:15:08 +0800

    提交

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String args[]){
+        String a = "asdf";
+        String ab = "asdf";
+        String abc = "asdf";
+        String abcd = "asdf33";
+        String abcdd = "asdf3322";
+        System.out.println(a.substring(1,3));
+        a.length();
+    }
+}
